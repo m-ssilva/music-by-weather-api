aeb820d583d5b58ec309c8ab63f92175e2f49f7a
Mateus Santos <mateus.santos@dextra-sw.com>
2019-10-15 11:06:03 -0300

    refactor(config): add spotify_api_host & api version

--- config/index.js
+++ config/index.js
@@ -1,12 +1,14 @@
 module.exports = {
   api: {
-    port: process.env.API_PORT || 3000
+    port: process.env.API_PORT || 3000,
+    version: 'v1'
   },
   weather_api: {
     token: '4c9093f427b537289b4ec79227c1aee1'
   },
   spotify: {
     authorization_host: 'https://accounts.spotify.com/api/token',
+    api_host: 'https://api.spotify.com/v1',
     client_id: '68d800ce23a441338b3aca1a4e6cc0fb',
     secret_key: 'dac112644cbd46e898d556107fd5404a'
   }
