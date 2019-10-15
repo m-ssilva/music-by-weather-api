3f5fc04aa1d5af45bcca3d7a9f559595e7f06f79
Mateus Santos <mateus.santos@dextra-sw.com>
2019-10-15 11:06:48 -0300

    refactor(app): remove no used dependencies

--- src/app.js
+++ src/app.js
@@ -5,13 +5,10 @@ const Router = new router()
 const path = require('path')
 const registerRoutes = require('./helpers/register-routes')
 const bodyParser = require('koa-bodyparser')
-const config = require('../config')
-const morgan = require('morgan')
 
 registerRoutes(Router, path.join(__dirname, './routes'))
 
 app.use(bodyParser())
-app.use(morgan('dev'))
 app.use(Router.routes())
 
 module.exports = app
