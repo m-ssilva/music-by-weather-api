5ef0377526b2037a769cabbcd53db2265132da5b
Mateus Santos <mateus.santos@dextra-sw.com>
2019-10-15 11:03:20 -0300

    feat(controller): add controller folder

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,10 @@
+const temperatureToCategory = temp => {
+  const parsedTemp = parseInt(temp)
+  if (parsedTemp > 25) return 'pop'
+  if (parsedTemp < 25 && parsedTemp > 10) return 'rock'
+  if (parsedTemp < 10) return 'classica'
+}
+
+module.exports = {
+  temperatureToCategory
+}
