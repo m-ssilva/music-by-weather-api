12429b1bbd9e4378f26ab8d8e301606d6a8dd591
m-ssilva <bleed-it-out@live.com>
2019-10-15 19:07:02 -0300

    fix(controller): temperature rule

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,7 @@
 const temperatureToCategory = temp => {
   const parsedTemp = parseInt(temp)
   if (parsedTemp > 25) return 'pop'
-  if (parsedTemp < 25 && parsedTemp > 10) return 'rock'
+  if (parsedTemp <= 25 && parsedTemp >= 10) return 'rock'
   if (parsedTemp < 10) return 'classical'
 }
 
