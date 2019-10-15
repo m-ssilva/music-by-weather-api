22f3bda0ebb2c1d0c05aac2592bf6ffbdbd22e57
Mateus Santos <mateus.santos@dextra-sw.com>
2019-10-15 11:08:27 -0300

    feat(weather): add openWeather service integration

--- src/services/weather.js
+++ src/services/weather.js
@@ -2,11 +2,14 @@ const axios = require('axios')
 const config = require('../../config')
 
 const getWeatherByCityName = name =>
-  axios
-    .get(
-      `http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&APPID=${config.weather_api.token}`
-    )
-    .catch(err => console.log(err))
+  axios({
+    method: 'get',
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&APPID=${config.weather_api.token}`
+  }).then(response => response.data)
+    .catch(err => {
+      if (err.response.status === 404) throw new Error('CITY_NOT_FOUND')
+      throw new Error('WEATHER_REQUEST_FAILED')
+    })
 
 module.exports = {
   getWeatherByCityName
