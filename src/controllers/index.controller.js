ec76731aaceb9b2efb38238767159d6ba41ec650
Mateus Santos <mateus.santos@dextra-sw.com>
2019-10-15 11:17:58 -0300

    fix(controller): rename classica to classical

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,7 +2,7 @@ const temperatureToCategory = temp => {
   const parsedTemp = parseInt(temp)
   if (parsedTemp > 25) return 'pop'
   if (parsedTemp < 25 && parsedTemp > 10) return 'rock'
-  if (parsedTemp < 10) return 'classica'
+  if (parsedTemp < 10) return 'classical'
 }
 
 module.exports = {
