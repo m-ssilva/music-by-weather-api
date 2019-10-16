66c55d4b0be772591fc21c879be970de468283fd
Mateus Santos <mateus.santos@dextra-sw.com>
2019-10-16 09:24:45 -0300

    feat(app): add logger

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ const Router = new router()
 const path = require('path')
 const registerRoutes = require('./helpers/register-routes')
 const bodyParser = require('koa-bodyparser')
+const logger = require('koa-logger')
 
 registerRoutes(Router, path.join(__dirname, './routes'))
 
 app.use(bodyParser())
+app.use(logger())
 app.use(Router.routes())
 
 module.exports = app
